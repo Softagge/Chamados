068f76c0d6d377a8135bfad93774ed36be7c0a6a
Fabio Aguiar <fabinhobarreto2@gmail.com>
2014-12-05 17:09:55 -0300

    primeiro commit 05/12/2013

---
+++ Chamados/src/IndexServlet.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse rep) {
+		try {
+			
+			PrintWriter out = rep.getWriter();
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Sistema de Chamados</title>");
+			out.println("</head>");
+			out.println("<h1>Sistema de Chamados</h1>");
+			out.println("<body>");
+			out.println("</body>");
+			out.println("</html>");
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse rep) {
+
+	}
+}
